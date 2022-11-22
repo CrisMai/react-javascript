4b4c72c2642866ce6a31bd9f27bd721f90ff3eab
CrisMai <crismai.dev@gmail.com>
2022-11-22 14:44:34 -0300

    Criado componente campo de texto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
 
       <Banner />
+      <CampoTexto />
 
     </div>
   );
