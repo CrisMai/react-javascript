43302b21be62c9f160a8c56ad805ef0f4a30e731
CrisMai <crismai.dev@gmail.com>
2022-11-28 15:41:16 -0300

    Criado card e componente colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,23 @@
+import './Colaborador.css';
+
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/crismai.png" alt="Cris Mai" />
+
+            </div>
+
+            <div className='rodape'>
+                <h4>Cris Mai</h4>
+                <h5>Estudante</h5>
+
+            </div>
+        </div>
+
+    )
+
+}
+
+export default Colaborador;
