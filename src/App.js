f25144947fef5ae3b6eec952dbdee575c7a15ff8
CrisMai <crismai.dev@gmail.com>
2022-11-30 14:28:58 -0300

    Criado componente rodapé

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario";
+import Rodape from "./componentes/Rodape";
 import Time from "./componentes/Time";
 
 function App() {
@@ -66,6 +67,8 @@ function App() {
           corSecundaria={time.corSecundaria}
           colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
         />)}
+      
+      <Rodape />
 
     </div>
   );
